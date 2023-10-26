d98fb4a7fb234f44d5baeadef73437fbb45d5c5e
Sudipto Kayal <sudipto.k@mucrest.com>
2023-10-26 18:33:05 +0530

    changes

--- src/pages/Recommendation.tsx
+++ src/pages/Recommendation.tsx
@@ -1,5 +1,13 @@
 import UnderConstruction from "../components/app/UnderConstruction";
-import { Typography } from "@mui/material";
+import {
+    Typography,
+    OutlinedInput,
+    FormControl,
+    InputLabel,
+    IconButton,
+    InputAdornment,
+} from "@mui/material";
+import SendRoundedIcon from "@mui/icons-material/SendRounded";
 import React from "react";
 
 function Recommendation() {
@@ -9,6 +17,35 @@ function Recommendation() {
                 Recommendation
             </Typography>
             <UnderConstruction />
+            <FormControl
+                fullWidth
+                sx={{
+                    m: 1,
+                    position: "sticky",
+                    bottom: "1rem",
+                    backgroundColor: "secondary.light",
+                }}
+            >
+                <InputLabel htmlFor="outlined-adornment-propmt">
+                    Send a propmt
+                </InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-propmt"
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                //   onClick={handleClickShowPassword}
+                                //   onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                            >
+                                <SendRoundedIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    label="Live Propmt"
+                />
+            </FormControl>
         </div>
     );
 }
